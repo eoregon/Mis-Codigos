2cd63d6002443d910a04c896b54678b34e04e9b7
Emilio O <42150142+eoregon@users.noreply.github.com>
2026-01-31 20:52:58 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
-	Name: 
+	Name: Emilio
 	Copyright: 
 	Author: 
 	Date: 06/10/17 16:47
@@ -10,9 +10,9 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int main() {
 	using namespace std;
-	cout.setf(ios_base::fixed, ios_base::floatfield); //arrego de punto decimal (6 digitos decimales)
+	cout.setf(ios_base::fixed, ios_base::floatfield); //arreglo de punto decimal (6 digitos decimales)
 	cout << "divisi\xA2n entera: 9/5 = " << 9 / 5 << endl << endl;
 	cout << "divisi\xA2n de punto flotante: 9.0/5.0 " << endl;
 	cout << 9.0 / 5.0 << endl << endl;
@@ -21,5 +21,7 @@ int main(int argc, char** argv) {
 	cout << 1.e7 / 9.0 << endl; // double por defecto
 	cout << "\nconstantes flotantes: 1e7f/9.0f = ";
 	cout << 1.e7f / 9.0 << endl;
+	
 	return 0;
 }
+
