a900242679fd9522fbe69c941f85e0a28f0215c1
Emilio O <42150142+eoregon@users.noreply.github.com>
2026-01-31 20:49:17 -0600

    Update Exceder.cpp

--- Exceder.cpp
+++ Exceder.cpp
@@ -9,28 +9,29 @@
 #include <iostream>
 #define CERO 0 // hacer que el simbolo Cero para el valor 0
 
-#include <climits> //define INT_MAX como el valor int m·s largo
+#include <climits> //define INT_MAX como el valor int m√°s largo
 
-int main(int argc, char** argv) {
+int main() {
 	using namespace std;
-	short Chester = SHRT_MAX; 		//inicializar con el valor m·ximo
-	unsigned short Silvia = Chester; 	//Esta bien si la variable sam ya est· definida
+	short Fulano = SHRT_MAX; 		//inicializar con el valor m√°ximo
+	unsigned short Fulana = Fulano; 	//Esta bien si la variable sam ya est√° definida
 	
-	cout << "Chester tiene " << Chester << " dolares y Silvia tiene " << Silvia;
+	cout << "Fulano tiene " << Fulano << " dolares y Fulana tiene " << Fulana;
 	cout << " dolares depositados." << endl << "Sumar $1 dolar a cada cuenta." << endl << "Ahora ";
-	Chester = Chester + 1;
-	Silvia = Silvia + 1;
-	cout << "Chester tiene " << Chester << " dolares y Silvia tiene " << Silvia;
-	cout << " dolares depositados.\nPobre de Chester!" << endl;
-	Chester = CERO;
-	Silvia = CERO;
-	cout << "Chester tiene " << Chester << " dolares y Silvia tiene  " << Silvia;
+	Fulano = Fulano + 1;
+	Fulana = Fulana + 1;
+	cout << "Fulano tiene " << Fulano << " dolares y Fulana tiene " << Fulana;
+	cout << " dolares depositados.\nPobre de Fulano!" << endl;
+	Fulano = CERO;
+	Fulana = CERO;
+	cout << "Fulano tiene " << Fulano << " dolares y Fulana tiene  " << Fulana;
 	cout << " doalres depositado." << endl;
 	cout << "Tomar $1 dolar de cada cuenta." << endl << "Ahora ";
-	Chester = Chester - 1;
-	Silvia = Silvia - 1;
-	cout << "Chester tiene " << Chester << " dolares y Silvia tiene " << Silvia;
-	cout << " dolares depositados." << endl << "Suerte de Silvia!" << endl;
+	Funalo = Fulano - 1;
+	Fulana = Fulana - 1;
+	cout << "Fulano tiene " << Fulano << " dolares y Silvia tiene " << Fulana;
+	cout << " dolares depositados." << endl << "Suerte de Fulana!" << endl;
 	/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 	return 0;
 }
+
